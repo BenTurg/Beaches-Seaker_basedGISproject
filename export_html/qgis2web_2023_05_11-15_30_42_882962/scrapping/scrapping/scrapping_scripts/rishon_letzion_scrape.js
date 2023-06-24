27b0f42dd1a706f4280c6b4e9e558b75c0198e30
BenTurg <76742555+BenTurg@users.noreply.github.com>
2023-06-24 20:53:18 +0300

    Update rishon_letzion_scrape.js
    update output to rishon json file

--- export_html/qgis2web_2023_05_11-15_30_42_882962/scrapping/scrapping/scrapping_scripts/rishon_letzion_scrape.js
+++ export_html/qgis2web_2023_05_11-15_30_42_882962/scrapping/scrapping/scrapping_scripts/rishon_letzion_scrape.js
@@ -112,7 +112,7 @@ async function scrapeWebsite() {
 
   // Write the data to a JSON file
   fs.writeFileSync(
-    "../json_output_files/rishon_letizon_output.json",
+    "export_html/qgis2web_2023_05_11-15_30_42_882962/scrapping/scrapping/json_output_files/rishon_letizon_output.json",
     JSON.stringify(desiredObject, null, 2)
   );
 
